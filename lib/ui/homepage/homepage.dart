a9ac3fc5398a05b23490c5385ebdfd24213aa809
Edouard Marquez <moi@edouard-marquez.me>
2025-05-27 12:16:04 +0200

    Ecran vide

---
+++ lib/ui/homepage/homepage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled4/res/app_colors.dart';
+import 'package:untitled4/res/app_icons.dart';
+import 'package:untitled4/res/app_vectorial_images.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.sizeOf(context).height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mes scans'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 8.0),
+              child: Icon(AppIcons.barcode),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(flex: 20),
+            SvgPicture.asset(AppVectorialImages.illEmpty),
+            Spacer(flex: 15),
+            Padding(
+              padding: const EdgeInsetsDirectional.symmetric(horizontal: 15.0),
+              child: Text(
+                'Vous n\'avez pas encore scanné de produit',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Spacer(flex: 10),
+            TextButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: AppColors.blue,
+                backgroundColor: AppColors.yellow,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(22.0)),
+                ),
+              ),
+              onPressed: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Commencer'.toUpperCase()),
+                  const SizedBox(width: 10.0),
+                  Icon(Icons.arrow_forward),
+                ],
+              ),
+            ),
+            Spacer(flex: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
